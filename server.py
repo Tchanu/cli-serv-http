fed3c656b0478716d6a8f2efd58eec839596abbf
tchanu <t.chanukvadze@gmail.com>
2017-07-14 23:46:27 +0400

    Switching to OOP

--- server.py
+++ server.py
@@ -3,49 +3,62 @@
 import socket
 from os import fork
 
-def init_connection():
-    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-    server_address=('localhost',3000)
-    sock.bind(server_address)
-    sock.listen(5)
-    return sock
 
-def read_html(file_requested):
-    try:
-        file_handler = open("static"+file_requested,'rb')
-        content = file_handler.read()
-        file_handler.close()
-        print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,addr[0],addr[1]))
-        return content
-    except Exception as e:
-        print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,addr[0],addr[1]))
-        return read_html("/404.html")
+class TCPServer():
+    def __init__(self,interface,port):
+        self.sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+        self.server_address=(interface,port)
+        print("Initialisation de la socket  \n ")
 
-sock=init_connection()
+        self.sock.bind(self.server_address)
+        self.sock.listen(5)
+        self.conn = None
+        print("Initialisation termine")
 
-while True:
-    print("En attente de connexions \n")
-    conn, addr=sock.accept()
-    pid=fork()
-    if pid==0:
-        sock.close()
+        self.url = None
+        self.protocol = None
+
+    def read_html(self,file_requested,client_address):
         try:
-            print ("\033[94m%s:%s connecté.\033[0m" % (addr[0],addr[1]))
-            while True:
-                data = conn.recv(1024)
-                data_string = bytes.decode(data)
-                method = data_string.split(' ')[0]
+            file_handler = open("static"+file_requested,'rb')
+            content = file_handler.read()
+            file_handler.close()
+            print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,client_address[0],client_address[1]))
+            return content
+        except Exception as e:
+            print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,client_address[0],client_address[1]))
+            file_handler = open("static/404.html",'rb')
+            content = file_handler.read()
+            file_handler.close()
+            return content
 
-                if (method == 'GET') | (method == 'HEAD'):
-                    file_requested = data_string.split(' ')[1]
-                    file_requested = file_requested.split('?')[0]
+    def run(self):
+        while True:
+            print("En attente de connexions \n")
+            self.conn,client_address = self.sock.accept()
+            pid=fork()
 
-                    content = read_html(file_requested)
+            if pid==0:
+                self.sock.close()
+                try:
+                    print ("\033[94m%s:%s connecté.\033[0m" % (client_address[0],client_address[1]))
+                    while True:
+                        data = self.conn.recv(1024)
+                        data_string = bytes.decode(data)
+                        method = data_string.split(' ')[0]
 
-                    conn.send(content)
-        finally:
-            exit()
-            conn.close()
-    else:
-        conn.close()
+                        if (method == 'GET') | (method == 'HEAD'):
+                            file_requested = data_string.split(' ')[1]
+                            content = self.read_html(file_requested.split('?')[0],client_address)
+
+                        self.conn.send(content)
+                finally:
+                    self.sock.close()
+                    exit()
+            else:
+                self.conn.close()
+
+if __name__ =='__main__':
+    tcps=TCPServer('localhost',3000)
+    tcps.run()
