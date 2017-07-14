85b9ba5fce9a69ab7065120faa50dc9af946732c
tchanu <t.chanukvadze@gmail.com>
2017-07-15 00:34:24 +0400

    Fixing 'self' errors

--- server.py
+++ server.py
@@ -18,50 +18,51 @@ class TCPServer():
         self.url = None
         self.protocol = None
 
-    def read_html(self,file_requested):
+    def get_html(self,file_requested):
         try:
             file_handler = open("static"+file_requested,'rb')
             content = file_handler.read()
             file_handler.close()
-            print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,self.client_address[0],self.client_address[1]))
+            print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,client_addr[0],client_addr[1]))
             return content
         except Exception as e:#404
-            print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,self.client_address[0],self.client_address[1]))
+            print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,client_addr[0],client_addr[1]))
             file_handler = open("static/404.html",'rb')
             content = file_handler.read()
             file_handler.close()
             return content
 
-    def read_http_req(self):
-        data = self.conn.recv(1024)
-        data_string = bytes.decode(data)
-        method = data_string.split(' ')[0]
+    def read_http_req(self,data):
+
+        method = data.split(' ')[0]
 
         if (method == 'GET') | (method == 'HEAD'):#supported methods
-            file_requested = data_string.split(' ')[1]
-            content = self.read_html(file_requested.split('?')[0])
-        else
-            content = self.read_html("/405.html")
+            file_requested = data.split(' ')[1]
+            content = self.get_html(file_requested.split('?')[0])
+        else:
+            content = self.get_html("/405.html")
         return content
 
     def run(self):
         while True:
             print("En attente de connexions \n")
-            self.conn,self.client_address = self.sock.accept()
+            conn,client_addr = self.sock.accept()
             pid=fork()
 
             if pid==0:
                 self.sock.close()
                 try:
-                    print ("\033[94m%s:%s connecté.\033[0m" % (self.client_address[0],self.client_address[1]))
+                    print ("\033[94m%s:%s connecté.\033[0m" % (client_addr[0],client_addr[1]))
                     while True:
-                        content = self.read_http_req()#HTTP request
+                        data = conn.recv(1024)
+                        data = bytes.decode(data)
+                        content = self.read_http_req(data)#HTTP request
                         conn.send(content)#Send content
                 finally:
                     self.sock.close()
                     exit()
             else:
-                self.conn.close()
+                conn.close()
 
 if __name__ =='__main__':
     tcps=TCPServer('localhost',3000)
