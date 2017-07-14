3756abe3fd237b9918f03f0d6828e1820fcebaf0
tchanu <t.chanukvadze@gmail.com>
2017-07-15 01:37:44 +0400

    ERROR 400
    Bad request

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 import socket
+import re
 from os import fork
 
 
@@ -15,30 +16,36 @@ class TCPServer():
         self.sock.listen(5)
         print("Initialisation termine")
 
-        self.url = None
-        self.protocol = None
 
     def get_html(self,file_requested):
         try:
             file_handler = open("static"+file_requested,'rb')
             content = file_handler.read()
             file_handler.close()
-            print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,client_addr[0],client_addr[1]))
+            print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,self.client_addr[0],self.client_addr[1]))
             return content
         except Exception as e:#404
-            print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,client_addr[0],client_addr[1]))
+            print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,self.client_addr[0],self.client_addr[1]))
             file_handler = open("static/404.html",'rb')
             content = file_handler.read()
             file_handler.close()
             return content
+    def is_valid_request(self,hostname):
+        allowed = re.compile(r"/(?!-)[//,\.,a-z0-9-]{1,63}$", re.IGNORECASE)
+        return allowed.match(hostname) != None
 
-    def read_http_req(self,data):
-
+    def http_req(self):
+        data = self.conn.recv(1024)
+        data = bytes.decode(data)
         method = data.split(' ')[0]
 
-        if (method == 'GET') | (method == 'HEAD'):#supported methods
+        if (method == 'GET') | (method == 'HEAD') | (method == 'PUT'):#supported methods
             file_requested = data.split(' ')[1]
-            content = self.get_html(file_requested.split('?')[0])
+            if(self.is_valid_request(file_requested)):
+                content = self.get_html(file_requested.split('?')[0])#everything ok
+            else:
+                content = self.get_html("/400.html")            
+
         else:
             content = self.get_html("/405.html")
         return content
@@ -46,23 +53,23 @@ class TCPServer():
     def run(self):
         while True:
             print("En attente de connexions \n")
-            conn,client_addr = self.sock.accept()
+            self.conn,self.client_addr = self.sock.accept()
             pid=fork()
 
             if pid==0:
                 self.sock.close()
                 try:
-                    print ("\033[94m%s:%s connecté.\033[0m" % (client_addr[0],client_addr[1]))
+                    print ("\033[94m%s:%s connecté.\033[0m" % (self.client_addr[0],self.client_addr[1]))
                     while True:
-                        data = conn.recv(1024)
-                        data = bytes.decode(data)
-                        content = self.read_http_req(data)#HTTP request
-                        conn.send(content)#Send content
+                        content = self.http_req()#HTTP request
+                        self.conn.send(content)#Send content
+                except Exception as e:
+                    print(e)
                 finally:
                     self.sock.close()
                     exit()
             else:
-                conn.close()
+                self.conn.close()
 
 if __name__ =='__main__':
     tcps=TCPServer('localhost',3000)
