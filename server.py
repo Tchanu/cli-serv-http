13535bc12187d0011eb5bb2dd3e994643a773f22
tchanu <t.chanukvadze@gmail.com>
2017-07-14 23:06:48 +0400

    added 404 page

--- server.py
+++ server.py
@@ -3,13 +3,25 @@
 import socket
 from os import fork
 
-def init_connection():  
+def init_connection():
     sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
     server_address=('localhost',3000)
     sock.bind(server_address)
     sock.listen(5)
     return sock
+
+def read_html(file_requested):
+    try:
+        file_handler = open("static"+file_requested,'rb')
+        content = file_handler.read()
+        file_handler.close()
+        print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,addr[0],addr[1]))
+        return content
+    except Exception as e:
+        print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,addr[0],addr[1]))
+        return read_html("/404.html")
+
 sock=init_connection()
 
 while True:
@@ -21,22 +33,15 @@ while True:
         try:
             print ("\033[94m%s:%s connecté.\033[0m" % (addr[0],addr[1]))
             while True:
-                data = conn.recv(1024) 
+                data = conn.recv(1024)
                 data_string = bytes.decode(data)
                 method = data_string.split(' ')[0]
 
                 if (method == 'GET') | (method == 'HEAD'):
                     file_requested = data_string.split(' ')[1]
                     file_requested = file_requested.split('?')[0]
- 
-                    try:                    
-                        file_handler = open("static"+file_requested,'rb')                       
-                        content = file_handler.read()
-                        file_handler.close()
-                        print ("\033[32m%s ---------------> %s:%s\033[0m" % (file_requested,addr[0],addr[1]))
-                    except Exception as e:
-                        content = "<html><title>ERROR 404</title><body>La page n'a pas été trouvée.</body></html>"
-                        print ("\033[31m%s(404) ---------------> %s:%s\033[0m" % (file_requested,addr[0],addr[1]))
+
+                    content = read_html(file_requested)
 
                     conn.send(content)
         finally:
